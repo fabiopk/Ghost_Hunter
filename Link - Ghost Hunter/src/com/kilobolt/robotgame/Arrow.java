047f8e6b62d9730774f58454633e325049a0214a
Fabio <fb8c@virginia.edu>
2014-04-05 13:11:40 -0400

    Added Project
    Added all files to the project

---
+++ Link - Ghost Hunter/src/com/kilobolt/robotgame/Arrow.java
@@ -0,0 +1,182 @@
+package com.kilobolt.robotgame;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Arrow {
+
+	private int xpos, ypos, xspeed, yspeed, ybonus, xbonus;
+	private boolean hit;
+
+	public Arrow(int x, int y) {
+		xspeed = 9;
+		yspeed = 9;
+		hit = false;
+		xpos = x;
+		ypos = y;
+	}
+
+	public void update() {
+		int[][] map = GameScreen.getTilemap();
+
+		xbonus += xspeed;
+		ybonus += yspeed;
+
+		if (xbonus <= -120) {
+			xpos -= 1;
+			xbonus = 0;
+			switch (map[xpos - 1][ypos]) {
+			case 0:
+				// Do nothing
+				break;
+
+			case 1:
+				hit = true;
+				break;
+
+			case 2:
+				// Do nothing
+				break;
+			case 3:
+				killGhost();
+				break;
+
+			}
+		}
+
+		if (xbonus >= 120) {
+			xpos += 1;
+			xbonus = 0;
+
+			switch (map[xpos + 1][ypos]) {
+			case 0:
+				// Do nothing
+				break;
+
+			case 1:
+				hit = true;
+				break;
+
+			case 2:
+				// Do nothing
+				break;
+			case 3:
+				killGhost();
+				break;
+
+			}
+		}
+
+		if (ybonus <= -120) {
+			ypos -= 1;
+			ybonus = 0;
+
+			switch (map[xpos][ypos - 1]) {
+			case 0:
+				// Do nothing
+				break;
+
+			case 1:
+				hit = true;
+				break;
+
+			case 2:
+				// Do nothing
+				break;
+			case 3:
+				killGhost();
+				break;
+
+			}
+		}
+
+		if (ybonus >= 120) {
+			ypos += 1;
+			ybonus = 0;
+
+			switch (map[xpos][ypos + 1]) {
+			case 0:
+				// Do nothing
+				break;
+
+			case 1:
+				hit = true;
+				break;
+
+			case 2:
+				// Do nothing
+				break;
+			case 3:
+				killGhost();
+				break;
+
+			}
+		}
+	}
+
+	private void killGhost() {
+		Iterator<Ghost> its = GameScreen.getGhosts().iterator();
+		while (its.hasNext()) {
+			Ghost ghost = its.next();
+				its.remove();
+		}
+
+	}
+
+	public int getXpos() {
+		return xpos;
+	}
+
+	public void setXpos(int xpos) {
+		this.xpos = xpos;
+	}
+
+	public int getYpos() {
+		return ypos;
+	}
+
+	public void setYpos(int ypos) {
+		this.ypos = ypos;
+	}
+
+	public int getXspeed() {
+		return xspeed;
+	}
+
+	public void setXspeed(int xspeed) {
+		this.xspeed = xspeed;
+	}
+
+	public int getYspeed() {
+		return yspeed;
+	}
+
+	public void setYspeed(int yspeed) {
+		this.yspeed = yspeed;
+	}
+
+	public int getYbonus() {
+		return ybonus;
+	}
+
+	public void setYbonus(int ybonus) {
+		this.ybonus = ybonus;
+	}
+
+	public int getXbonus() {
+		return xbonus;
+	}
+
+	public void setXbonus(int xbonus) {
+		this.xbonus = xbonus;
+	}
+
+	public boolean isHit() {
+		return hit;
+	}
+
+	public void setHit(boolean hit) {
+		this.hit = hit;
+	}
+
+}
